f4535dbd771ce7d6f01cc65233e50eddfddd1370
Georgi <slaweykow@protonmail.com>
2021-02-12 03:59:44 +0200

    Fixing the paths for the animations

--- application/lib/ui/util/app_animations.dart
+++ application/lib/ui/util/app_animations.dart
@@ -1,16 +1,16 @@
 class AppAnimations {
   static final String distance =
-      "https://raw.githubusercontent.com/nathan1us/CovidInfo/master/assets/json/distance.json";
+      "https://raw.githubusercontent.com/nathan1us/CovidInfo/main/application/assets/json/distance.json";
   static final String handsWashing =
-      "https://raw.githubusercontent.com/nathan1us/CovidInfo/master/assets/json/handsWashing.json";
+      "https://raw.githubusercontent.com/nathan1us/CovidInfo/main/application/assets/json/handsWashing.json";
   static final String highTemp =
-      "https://raw.githubusercontent.com/nathan1us/CovidInfo/master/assets/json/highTemp.json";
+      "https://raw.githubusercontent.com/nathan1us/CovidInfo/main/application/assets/json/highTemp.json";
   static final String sneezing =
-      "https://raw.githubusercontent.com/nathan1us/CovidInfo/master/assets/json/sneezing.json";
+      "https://raw.githubusercontent.com/nathan1us/CovidInfo/main/application/assets/json/sneezing.json";
   static final String disinfection =
-      "https://raw.githubusercontent.com/nathan1us/CovidInfo/master/assets/json/disinfection.json";
+      "https://raw.githubusercontent.com/nathan1us/CovidInfo/main/application/assets/json/disinfection.json";
   static final String maskWearing =
-      "https://raw.githubusercontent.com/nathan1us/CovidInfo/master/assets/json/maskWearing.json";
+      "https://raw.githubusercontent.com/nathan1us/CovidInfo/main/application/assets/json/maskWearing.json";
   static final String cold =
-      "https://raw.githubusercontent.com/nathan1us/CovidInfo/master/assets/json/cold.json";
+      "https://raw.githubusercontent.com/nathan1us/CovidInfo/main/application/assets/json/cold.json";
 }
